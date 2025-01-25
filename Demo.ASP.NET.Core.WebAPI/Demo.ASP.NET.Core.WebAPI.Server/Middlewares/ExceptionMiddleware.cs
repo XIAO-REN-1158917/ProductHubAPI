ed684acc5ed5043a04264977390e58ea37255de1
Gary Ren <espresso1014@gmail.com>
2025-01-25 14:11:03 +1300

    other checks

--- Demo.ASP.NET.Core.WebAPI/Demo.ASP.NET.Core.WebAPI.Server/Middlewares/ExceptionMiddleware.cs
+++ Demo.ASP.NET.Core.WebAPI/Demo.ASP.NET.Core.WebAPI.Server/Middlewares/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo.ASP.NET.Core.WebAPI.Server.Middlewares
+﻿using Demo.ASP.NET.Core.WebAPI.Server.Exceptions;
+
+namespace Demo.ASP.NET.Core.WebAPI.Server.Middlewares
 {
     public class ExceptionHandlingMiddleware
     {
@@ -13,28 +15,35 @@
 
         public async Task InvokeAsync(HttpContext context)
         {
-            try
-            {
-                await _next(context);
-            }
-            catch (ArgumentException ex)
-            {
-                _logger.LogWarning(ex, "Validation error.");
-                context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsJsonAsync(new { message = ex.Message });
-            }
-            catch (InvalidOperationException ex)
-            {
-                _logger.LogWarning(ex, "Business logic error.");
-                context.Response.StatusCode = StatusCodes.Status404NotFound;
-                await context.Response.WriteAsJsonAsync(new { message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Unexpected error occurred.");
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsJsonAsync(new { message = "An unexpected error occurred." });
-            }
+            await _next(context);
+            //try
+            //{
+            //    await _next(context);
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    _logger.LogWarning(ex, "Validation error.");
+            //    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            //    await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+            //}
+            //catch (InvalidOperationException ex)
+            //{
+            //    _logger.LogWarning(ex, "Business logic error.");
+            //    context.Response.StatusCode = StatusCodes.Status404NotFound;
+            //    await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+            //}
+            ////catch (NullReferenceException ex)
+            ////{
+            ////    _logger.LogWarning(ex, "Business logic error.");
+            ////    context.Response.StatusCode = StatusCodes.Status404NotFound;
+            ////    await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+            ////}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "Unexpected error occurred.");
+            //    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            //    await context.Response.WriteAsJsonAsync(new { message = "An unexpected error occurred." });
+            //}
         }
     }
 
